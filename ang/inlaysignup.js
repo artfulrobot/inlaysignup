ac99b1e867a70be2102e88b02d62ee4271c283b3
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2020-10-26 17:40:27 +0000

    botched attempt to add angular page

--- ang/inlaysignup.js
+++ ang/inlaysignup.js
@@ -1,4 +1,5 @@
 (function(angular, $, _) {
   // Declare a list of dependencies.
   angular.module('inlaysignup', CRM.angRequires('inlaysignup'));
+  angular.module('inlaycontact', CRM.angRequires('inlaycontact'));
 })(angular, CRM.$, CRM._);
