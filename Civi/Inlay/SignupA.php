20aebbab60153d401708165c40058a9e0d649c17
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2022-07-04 15:12:00 +0100

    Fix main branch signatures for inlay 1.3

--- Civi/Inlay/SignupA.php
+++ Civi/Inlay/SignupA.php
@@ -42,26 +42,11 @@ class SignupA extends InlayType {
   public static $editURLTemplate = 'civicrm/a?#/inlays/signupA/{id}';
 
   /**
-   * Sets the config ensuring it's valid.
-   *
-   * This implementation simply ensures all the defaults exist, and that no
-   * other keys exist, but you could do other things, especially if you need to
-   * coerce some old config into a new style.
-   *
-   * @param array $config
-   *
-   * @return \Civi\Inlay\Type (this)
-   */
-  public function setConfig(array $config) {
-    $this->config = array_intersect_key($config + static::$defaultConfig, static::$defaultConfig);
-  }
-
-  /**
    * Generates data to be served with the Javascript application code bundle.
    *
    * @return array
    */
-  public function getInitData() {
+  public function getInitData() :array {
     $data = [
       // Name of global Javascript function used to boot this app.
       'init'             => 'inlaySignupAInit',
@@ -119,7 +104,7 @@ class SignupA extends InlayType {
    *
    * @throws \Civi\Inlay\ApiException;
    */
-  public function processRequest(ApiRequest $request) {
+  public function processRequest(ApiRequest $request) :array {
 
     $data = $this->cleanupInput($request->getBody());
 
@@ -257,7 +242,7 @@ class SignupA extends InlayType {
    *
    * @return string Content of a Javascript file.
    */
-  public function getExternalScript() {
+  public function getExternalScript() :string {
     return file_get_contents(E::path('dist/inlay-signup-a.js'));
   }
 
