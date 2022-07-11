c69493453f000cf63c18dd1575a346744d279373
Rich Lott / Artful Robot <code.commits@artfulrobot.uk>
2022-07-11 12:03:06 +0100

    co: Support German translation for specific hard-coded inlays

--- src/coDownload.js
+++ src/coDownload.js
@@ -2,6 +2,57 @@
 import './coDownload.scss';
 
 (() => {
+
+  let langMap = null;
+
+  let ende = {
+    'Download' : 'Download',
+    'Close this form' : 'Fenster schließen',
+    'required' : 'verpflichtend',
+    'First name' : 'Vorname',
+    'Last name' : 'Nachname',
+    'Email' : 'E-Mail Adresse',
+    'youremail@example.org' : 'ihremail@example.org',
+    'Email again to confirm' : 'E-Mail Adresse bestätigen',
+    'Organisation' : 'Organisation',
+    'Please select' : 'Bitte auswählen',
+    'Yes' : 'Ja',
+    'No' : 'Nein',
+    'Thank you.' : 'Vielen Dank ',
+    'Just a mo...' : 'Einen Augenblick...',
+    'Your download should begin automatically after a few seconds. If not use this <a>direct download link</a>':
+      'Ihr Download sollte nach wenigen Sekunden automatisch beginnen. Falls nicht, verwenden Sie bitte diesen <a>direkten Download-Link</a>',
+    'Please check your email; the two don’t match' : 'Keine Übereinstimmung; bitte überprüfen Sie Ihre E-Mail Adresse',
+    'Sorry, there was a problem with the form' : 'Entschuldigung, es gab ein Problem mit dem Onlineformular',
+
+    'It would be very helpful to understand how and where you intend to use these insights - could you give us a sense please?' : 'Wir würden gerne besser verstehen, wie unsere Handreichung für Sie nützlich sein kann. Könnten Sie uns mitteilen, wie Sie die Erkenntnisse in Ihrer Arbeit zu nutzen gedenken?',
+    '<h2>We hope you find this resource&nbsp;useful -&nbsp;please do let us know what you think!</h2>' : '<h2>Wir hoffen, dass Sie diese Handreichung nützlich finden - wir freuen uns über Rückmeldungen!</h2>',
+
+    'We\'d truly appreciate your support in helping us ensure our resources are as impactful as possible. May we send you a few questions about how you\'ve used the resource in a couple of months?':
+      'Ihr Feedback hilft uns dabei, unsere Ressourcen so anwendungsorientiert wie möglich zu gestalten. Dürfen wir Ihnen in ein paar Monaten ein paar Fragen zusenden, um zu erfahren, wie Sie diese Handreichung genutzt haben?',
+    '<p>We will store, process and manage your information according to our <a href="https://climateoutreach.org/privacy-notice/">privacy policy</a>.&nbsp;</p>':
+      '<p>Wir speichern, verarbeiten und verwalten Ihre Daten gemäß unserer <a href="https://climateoutreach.org/privacy-notice/">Datenschutzrichtlinie</a>.</p>'
+  };
+
+  // Call like: ts('My name is %1', {1: 'Lorem'});
+  function ts(english, params) {
+    let translated = english;
+
+    if (langMap) {
+      // We expect a translation
+      if (!(english in langMap)) {
+        console.warn("Missing translation for", {english});
+      }
+      else {
+        translated = langMap[english];
+        console.log("✔ translation for", {english, translated});
+      }
+    }
+    // Translated or not, we need substitutions.
+    Object.keys(params || {}).forEach(prop => { translated.replace('%' + prop, params[prop]); });
+    return translated;
+  }
+
   if (!window.inlayCoDownloadInit) {
     // This is the first time this *type* of Inlay has been encountered.
     // We need to define anything global here.
@@ -93,7 +144,7 @@ import './coDownload.scss';
       };
 
 
-      // We need to add button after each <noscript data-inlay-id={ourID}>
+      // We need to add button after each <noscript data-inlay-id={ourID} data-download-title={title} data-download-id={id}>
       [].forEach.call(document.querySelectorAll(`noscript[data-inlay-id="${inlay.publicID}"]`), (el, index, array) => {
         if (el.inlayProcessed) {
           return;
@@ -104,24 +155,36 @@ import './coDownload.scss';
         // Copy classes from noscript tag to enable local styling.
         // Provide defaults.
         btn.className = el.className || 'button button--orange';
+        // When button clicked, call handleInputInterraction passing the original <noscript> node, el.
         btn.addEventListener('click', e => { handleInputInterraction(null, el) } );
         btn.innerHTML = 'Download<svg class="icon"><use xlink:href="#icon--button"></use></svg>';
         el.insertAdjacentElement('afterend', btn);
+
+        // Language hack!
+        // If a particular report ID is found, switch to German.
+        // console.log("XXX ", {el, ds: el.dataset, id: el.dataset.downloadId});
+        if (el.dataset.downloadId === '27530') {
+          console.log("Switched to German");
+          langMap = ende;
+        }
       });
 
       // Here we create the download form and put it on the page.
       const downloadAppDiv = document.createElement('div');
       // We append 'i' to the inlayID because classes can't start with a number.
       downloadAppDiv.classList.add('inlay-download', 'i' + inlay.publicID, 'at-rest');
+      // We need to know the language.
+
+
       downloadAppDiv.innerHTML = `
       <div class="idl-overlay">
         <div class="idl-locator">
-          <button class="idl-close" title="Close this form">×</button>
+          <button class="idl-close" title="${ts('Close this form')}">×</button>
           <form action='#' >
             <h2 class="idl-title"></h2>
             <div class="idl-warning-placeholder"></div>
             <div class="idl-field first_name">
-              <label for="${uniquePrefix}-fn">First name<sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-fn">${ts('First name')}<sup class="red" title="${ts('required')}">*</sup></label>
               <input
                 id="${uniquePrefix}-fn"
                 name="first_name"
@@ -131,7 +194,7 @@ import './coDownload.scss';
             </div>
 
             <div class="idl-field last_name">
-              <label for="${uniquePrefix}-ln">Last name<sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-ln">${ts('Last name')}<sup class="red" title="${ts('required')}">*</sup></label>
               <input
                 id="${uniquePrefix}-fn"
                 name="last_name"
@@ -141,18 +204,18 @@ import './coDownload.scss';
             </div>
 
             <div class="idl-field email">
-              <label for="${uniquePrefix}-e1">Email<sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-e1">${ts('Email')}<sup class="red" title="${ts('required')}">*</sup></label>
               <input
                 id="${uniquePrefix}-e1"
                 name="email"
                 type="email"
-                placeholder="youremail@example.org"
+                placeholder="${ts('youremail@example.org')}"
                 required
                 />
             </div>
 
             <div class="idl-field email">
-              <label for="${uniquePrefix}-e2">Email again to confirm<sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-e2">${ts('Email again to confirm')}<sup class="red" title="${ts('required')}">*</sup></label>
               <input
                 id="${uniquePrefix}-e1"
                 name="email2"
@@ -162,7 +225,7 @@ import './coDownload.scss';
             </div>
 
             <div class="idl-field organisation">
-              <label for="${uniquePrefix}-o">Organisation</label>
+              <label for="${uniquePrefix}-o">${ts('Organisation')}</label>
               <input
                 id="${uniquePrefix}-o"
                 name="organisation"
@@ -172,7 +235,7 @@ import './coDownload.scss';
             </div>
 
             <div class="idl-field question">
-              <label for="${uniquePrefix}-q"><span></span><sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-q"><span></span><sup class="red" title="${ts('required')}">*</sup></label>
               <textarea
                 rows=6 cols=60
                 id="${uniquePrefix}-q"
@@ -182,11 +245,11 @@ import './coDownload.scss';
             </div>
 
             <div class="idl-field followup">
-              <label for="${uniquePrefix}-fu"><span></span><sup class="red" title="required">*</sup></label>
+              <label for="${uniquePrefix}-fu"><span></span><sup class="red" title="${ts('required')}">*</sup></label>
               <select id="${uniquePrefix}-fu" required name="followup" >
-                <option value="">--Please select--</option>
-                <option value="Yes">Yes</option>
-                <option value="No">No</option>
+                <option value="">--${ts('Please select')}--</option>
+                <option value="Yes">${ts('Yes')}</option>
+                <option value="No">${ts('No')}</option>
               </select>
             </div>
 
@@ -194,16 +257,15 @@ import './coDownload.scss';
             <div class="idl-buttons">
               <div class="idl-smallprint" ></div>
               <button class="idl-submit button button--orange" type="submit" >
-                <span>Download</span>
+                <span>${ts('Download')}</span>
                 <svg class="icon"><use xlink:href="#icon--button"></use></svg>
               </button>
               <div class="idl-progress-container"><div class="idl-progress"></div></div>
             </div>
           </form>
           <div class="idl-thanks">
-            <div class="text">Thank you.</div>
-            <p>Your download should begin automatically after a few seconds. If not use this
-              <a>direct download link</a></p>
+            <div class="text">${ts('Thank you.')}</div>
+            <p>${ts('Your download should begin automatically after a few seconds. If not use this <a>direct download link</a>')}</p>
           </div>
         </div>
       </div>
@@ -233,14 +295,14 @@ import './coDownload.scss';
       };
 
       // Set up the thanks and hide it.
-      nodes.thanksText.innerHTML = inlay.initData.webThanksHTML;
+      nodes.thanksText.innerHTML = ts(inlay.initData.webThanksHTML);
       nodes.thanks.style.display = 'none';
       // Set button text
       nodes.submitButtonText.textContent = inlay.initData.buttonText;
       // Set up other texts
-      nodes.smallprint.innerHTML = inlay.initData.smallprintHTML;
-      nodes.questionLabel.textContent = inlay.initData.questionText;
-      nodes.followupLabel.textContent = inlay.initData.followupText;
+      nodes.smallprint.innerHTML = ts(inlay.initData.smallprintHTML);
+      nodes.questionLabel.textContent = ts(inlay.initData.questionText);
+      nodes.followupLabel.textContent = ts(inlay.initData.followupText);
 
       const allInputs = ['emailInput', 'firstNameInput', 'lastNameInput', 'email2Input', 'orgInput'].map(i => nodes[i]);
       // Provide ways to close the overlay.
@@ -253,7 +315,7 @@ import './coDownload.scss';
       // Initial state: don't show validation errors.
       allInputs.forEach(i => {
         // Inputs have a .pre-interaction class until they've been interacted
-        // with, or the form has been submitted.
+        // with, or the form has been subitted.
         i.classList.add('pre-interaction');
         i.addEventListener('input', e => handleInputInterraction(i));
         i.addEventListener('focus', e => handleInputInterraction(i));
@@ -266,7 +328,7 @@ import './coDownload.scss';
         allInputs.forEach(i => handleInputInterraction);
         // Invalidate if emails don't match.
         if (nodes.email2Input.value !== nodes.emailInput.value) {
-          alert("Please check your email; the two don't match");
+          alert(ts('Please check your email; the two don’t match'));
           // Prevent form submission.
           e.preventDefault();
           return;
@@ -336,13 +398,13 @@ import './coDownload.scss';
         // todo validation in case browser validation is not supported.
 
         nodes.submitButton.disabled = true;
-        nodes.submitButton.textContent = 'Just a mo...';
+        nodes.submitButton.textContent = ts('Just a mo...');
         // Expect it to take 2s to do first 20%
         startTimer(2, 20, 1);
 
         function cancelSubmission() {
           nodes.submitButton.disabled = false;
-          nodes.submitButtonText.textContent = inlay.initData.buttonText;
+          nodes.submitButtonText.textContent = ts(inlay.initData.buttonText);
           cancelTimer();
         }
 
@@ -352,7 +414,7 @@ import './coDownload.scss';
             return;
           }
           else if (r.error) {
-            alert("Sorry, there was a problem with the form: " + r.error);
+            alert(ts("Sorry, there was a problem with the form") + ' :' + r.error);
             cancelSubmission();
           }
           else if (r.token) {
@@ -374,7 +436,7 @@ import './coDownload.scss';
                   window.location = '/download/' + report.id;
                 }
                 else {
-                  alert("Sorry, there was a problem with the form: " + (r.error || 'unknown error'));
+                  alert(ts("Sorry, there was a problem with the form") + ' :' + (r.error || 'unknown error'));
                   cancelSubmission();
                   return;
                 }
